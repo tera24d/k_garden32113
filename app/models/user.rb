d95de5caf9e4c820ee21809d748db0d58f9f85c8
tera24d <tera12d@gmail.com>
2020-12-11 13:23:47 +0900

    新規登録の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,20 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-         extend ActiveHash::Associations::ActiveRecordExtensions
-         belongs_to :sex
-         belongs_to :prefecture
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :sex
+  belongs_to :prefecture
          
-         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-         validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
               
-         validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }
                 
-         with_options presence: true do
-           validates :nickname
-           with_options numericality: { other_than: 0, message: 'select' } do
-             validates :sex_id
-             validates :prefecture_id
-           end
-         end
+  with_options presence: true do
+    validates :nickname
+    with_options numericality: { other_than: 0, message: 'select' } do
+      validates :sex_id
+      validates :prefecture_id
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'events/index'
   root to: "events#index"
 end
